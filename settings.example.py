27e018f9ac1b3a6b57bf32257c215a075557cb93
Exile <csxkingjp@gmail.com>
2023-11-04 17:37:59 -0600

    - Added archive functionality (not complete but functional) - Added initialization section where the app ensures correct files/folders exist.

--- settings.example.py
+++ settings.example.py
@@ -1,6 +1,10 @@
 class CBotSettings:
     # Your Discord bot token goes here
     TOKEN = "YOUR DISCORD TOKEN HERE"
+    # What channel should the bot log admin/mod actions it performs to?
+    CHANNEL_ID = 0000000000000000000
+    # Which character should we use to trigger a command?
+    CommandSymbol = "$"
     # Should we archive images?
     ArchiveImages = False
     # If `ArchiveImages` is enabled, which channels should we archive?
