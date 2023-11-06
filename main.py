90cd764f39899e4991854a6c6a0f5ca41a26377a
Exile <csxkingjp@gmail.com>
2023-11-06 01:35:37 -0700

    test 4

--- main.py
+++ main.py
@@ -34,7 +34,8 @@ def CheckForUpdate():
         stdout, stderr = update.communicate()
         print(stdout)
         print(f"[UPDATER] Update complete! Relaunching...")
-        os.exec(sys.argv[0], sys.argv)
+        #os.exec(sys.argv[0], sys.argv)
+        os.execv(sys.executable, ['python'] + [sys.argv[0], sys.argv])
 
 CheckForUpdate()
 
